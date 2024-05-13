e68258247d459f848594ea3704f601a232b59f98
Tyler Yu <tyleryy@uci.edu>
2024-05-13 00:44:53 -0700

    feat: readded serviceAccountKey

--- packages/functions/src/config/firebase-config.js
+++ packages/functions/src/config/firebase-config.js
@@ -1,9 +1,9 @@
-// var admin = require("firebase-admin");
+var admin = require("firebase-admin");
 
-// var serviceAccount = require("./serviceAccountKey.json");
+var serviceAccount = require("./serviceAccountKey.json");
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-// module.exports = admin;
+module.exports = admin;
