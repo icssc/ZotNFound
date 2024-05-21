b52e5905edbdb09814e8a6f36df31ba7061c45cf
Dang Nguyen Nguyen <93059380+NwinNwin@users.noreply.github.com>
2024-05-20 17:30:12 -0700

    fixed returned item in filter doesn't work

--- packages/web/src/utils/Utils.js
+++ packages/web/src/utils/Utils.js
@@ -1,13 +1,15 @@
-  // Define callback function to return filtered items (filtered according to search bar and filter markers)
-  const filterItem = (item, findFilter, user) => {
-      return (((findFilter.islost && item.islost) ||
-          (findFilter.isFound && !item.islost)) &&
-          (findFilter.type === "everything" || findFilter.type === item.type) &&
-          (findFilter.uploadDate === "" ||
-            (item.itemDate && item.itemDate.includes(findFilter.uploadDate))) &&
-          (!findFilter.isYourPosts || item.email === user.email) &&
-          (findFilter.isShowReturned || !item.isResolved)
-      );
-    };
+// Define callback function to return filtered items (filtered according to search bar and filter markers)
+const filterItem = (item, findFilter, user) => {
+  console.log(item);
+  return (
+    ((findFilter.islost && item.islost) ||
+      (findFilter.isFound && !item.islost)) &&
+    (findFilter.type === "everything" || findFilter.type === item.type) &&
+    (findFilter.uploadDate === "" ||
+      (item.itemDate && item.itemDate.includes(findFilter.uploadDate))) &&
+    (!findFilter.isYourPosts || item.email === user.email) &&
+    (findFilter.isShowReturned || !item.isresolved)
+  );
+};
 
-  export { filterItem };
+export { filterItem };
