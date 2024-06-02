f35b0bdaa5812766cd563a58bc20b8c46f8dd802
tentsundue <tentsundue28@gmail.com>
2024-06-01 17:02:07 -0700

    commented out item logging for now

--- packages/web/src/utils/Utils.js
+++ packages/web/src/utils/Utils.js
@@ -1,6 +1,6 @@
 // Define callback function to return filtered items (filtered according to search bar and filter markers)
 const filterItem = (item, findFilter, user) => {
-  console.log(item);
+  // console.log(item);
   return (
     ((findFilter.islost && item.islost) ||
       (findFilter.isFound && !item.islost)) &&
