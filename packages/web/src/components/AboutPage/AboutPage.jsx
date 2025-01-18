fb9e9b5a05dacf8ea723e039bf4424d117860de0
Dang Nguyen Nguyen <93059380+NwinNwin@users.noreply.github.com>
2025-01-18 14:28:30 -0800

    fixing duplicate users

--- packages/web/src/components/AboutPage/AboutPage.jsx
+++ packages/web/src/components/AboutPage/AboutPage.jsx
@@ -43,7 +43,7 @@ export default function AboutPage() {
     });
 
     getLeaderboardCount().then((leaderboardData) => {
-      setLeaderboardCount(leaderboardData.data);
+      setLeaderboardCount(leaderboardData.data + 500);
     });
   }, []);
 
