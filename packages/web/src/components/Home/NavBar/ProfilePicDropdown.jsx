59ceac63b40b06b9c051c111765aff8ccf736eb2
rebecca-123 <rebecca.notw@gmail.com>
2025-03-29 15:22:58 -0700

    remove subscription code

--- packages/web/src/components/Home/NavBar/ProfilePicDropdown.jsx
+++ packages/web/src/components/Home/NavBar/ProfilePicDropdown.jsx
@@ -10,10 +10,6 @@ import {
 
 import logout from "../../../assets/logos/logout.svg";
 import logout_white from "../../../assets/logos/logout_white.svg";
-import subscribe from "../../../assets/logos/subscribe.svg";
-import subscribe_white from "../../../assets/logos/subscribe_white.svg";
-import unsubscribe from "../../../assets/logos/unsubscribe.svg";
-import unsubscribe_white from "../../../assets/logos/unsubscribe_white.svg";
 import userlogo from "../../../assets/logos/userlogo.svg";
 import userlogo_white from "../../../assets/logos/userlogo_white.svg";
 import yourposts from "../../../assets/logos/yourposts.svg";
@@ -21,9 +17,7 @@ import yourposts_white from "../../../assets/logos/yourposts_white.svg";
 
 export default function ProfilePicDropdown({
   user,
-  subscription,
   handleLogout,
-  subscribeToggle,
   setFindFilter,
   onOpen,
 }) {
@@ -67,30 +61,6 @@ export default function ProfilePicDropdown({
             mr="12px"
           />
           Your Posts
-        </MenuItem>
-
-        <MenuItem onClick={subscribeToggle}>
-          {subscription ? (
-            <>
-              <Image
-                boxSize="1.2rem"
-                src={colorMode === "dark" ? unsubscribe_white : unsubscribe}
-                alt="Unsubscribe from newsletter button"
-                mr="12px"
-              />
-              Unsubscribe
-            </>
-          ) : (
-            <>
-              <Image
-                boxSize="1.2rem"
-                src={colorMode === "dark" ? subscribe_white : subscribe}
-                alt="Subscribe from newsletter button"
-                mr="12px"
-              />
-              Subscribe
-            </>
-          )}
         </MenuItem>
 
         <MenuItem onClick={handleLogout}>
